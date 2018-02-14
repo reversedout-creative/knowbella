156c449ac4062ee18ddb71db6a91ff40bed2f4aa
Alex <davis.alex.jr@gmail.com>
2018-02-13 22:53:18 -0500

    Working on mobile team section

--- js/custom.js
+++ js/custom.js
@@ -95,3 +95,19 @@
 	});
 
 })(jQuery);
+
+var acc = document.getElementsByClassName("team-btn");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+	var panel = this.nextElementSibling;
+	console.log(panel);
+    if (panel.style.maxHeight){
+      panel.style.maxHeight = null;
+    } else {
+      panel.style.maxHeight = panel.scrollHeight + "px";
+    } 
+  });
+}
